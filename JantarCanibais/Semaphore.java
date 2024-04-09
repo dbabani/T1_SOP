88f6c6468b59e8e53851bc55fea811a85e51abc1
Dhruv Babani <120258006+dbabani@users.noreply.github.com>
2024-04-09 19:58:15 -0300

    Update Semaphore.java

--- JantarCanibais/Semaphore.java
+++ JantarCanibais/Semaphore.java
@@ -12,12 +12,5 @@ class Semaphore {
         notify();
     }
 
-    public synchronized void down() throws InterruptedException {
-        while (value <= 0){
-            wait();
-        }
-            
-        value--;
-        notify(); // Notificar os canibais que o recurso foi atualizado
-    }
+    
 }
